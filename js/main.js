dfb00dbd1823007c72fa1d4c856e0673d5acad8b
bekmirza-pro <bekmirzaprogrammer@yandex.ru>
2021-07-14 18:41:11 -0700

    who go first last commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var elForm = document.querySelector(".form");
-var elUserInput = elForm.querySelector(".user-input");
+var elDistanceInput = elForm.querySelector(".distance-input");
 var elResultBox1 = document.querySelector(".paragraf1");
 var elResultBox2 = document.querySelector(".paragraf2");
 var elResultBox3 = document.querySelector(".paragraf3");
@@ -10,14 +10,29 @@ var speedBike = 20.1;
 var speedCar = 70;
 var speedPlane = 800;
 
+function calculateTime(distance, speed){
+  var hour = Math.floor(distance / speed);
+  var minute = Math.floor((distance / speed - hour) * 60);
+  var secound = Math.floor(((distance / speed - hour) * 60 - minute) * 60);
+  return hour + " soat " + minute + " minut " + secound + " secund "
+
+}
 
 
 elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
   
-    elResultBox1.textContent = (Number(elUserInput.value.trim()) / speedMan).toFixed(2);
-    elResultBox2.textContent = (Number(elUserInput.value.trim()) / speedBike).toFixed(2);
-    elResultBox3.textContent = (Number(elUserInput.value.trim()) / speedCar).toFixed(2);
-    elResultBox4.textContent = (Number(elUserInput.value.trim()) / speedPlane).toFixed(2);
+    var distanceInput = Number(elDistanceInput.value.trim())
+
+    elResultBox1.textContent = calculateTime(distanceInput, speedMan);
+    elResultBox2.textContent = calculateTime(distanceInput, speedBike);
+    elResultBox3.textContent = calculateTime(distanceInput, speedCar);
+    elResultBox4.textContent = calculateTime(distanceInput, speedPlane);
+
+    // elResultBox1.textContent = (Number(elUserInput.value.trim()) / speedMan).toFixe(2);
+    // elResultBox2.textContent = (Number(elUserInput.value.trim()) / speedBike).toFixe(2);
+    // elResultBox3.textContent = (Number(elUserInput.value.trim()) / speedCar).toFixe(2);
+    // elResultBox4.textContent = (Number(elUserInput.value.trim()) / speedPlane).toFixe(2);
     
   });
+
